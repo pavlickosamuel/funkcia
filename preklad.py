a7c5a543007809de8cf928cf48c33a9d877c50d5
pavlickosamuel <pavlickosamuel@gmail.com>
2025-11-12 09:27:38 +0100

    preklad

---
+++ preklad.py
@@ -0,0 +1,19 @@
+def translation(text):
+    vowels = "aeiouAEIOU"
+    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+    translated = ""
+    counter = 0
+    while counter < len(text):
+        char = text[counter]
+        if char in vowels:
+            counter += 3
+            translated += char
+        elif char in consonants:
+            counter += 2
+            translated += char
+        else:
+            counter += 1
+            translated += char
+    return translated
+preklad = translation("hieeelalaooo")
+print(preklad)
